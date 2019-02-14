8fbebbe9375cccc5c85c8f5a03f46a5640ceb751
Jacques Cornat <cornat.jacques@gmail.com>
2019-02-14 19:16:58 +0100

    add get api

--- app/controller/pdf.ts
+++ app/controller/pdf.ts
@@ -3,6 +3,19 @@ import { Application } from 'express';
 import * as PDF from '../model/pdf';
 
 export function init(app: Application): void {
+  app.get('/api/convert', async (req: any, res: any, next: any) => {
+    const html = req.params.html;
+
+    try {
+      const data = await PDF.convertHtmlContentToPDF(html);
+      res.send(data);
+    } catch (error) {
+      return next(error);
+    }
+
+    next();
+  });
+
   app.post('/api/convert', async (req: any, res: any, next: any) => {
     const html = req.body.html;
 
