37e7eca941a0ed9285603b5246ec55c5cbd4ea70
Jacques Cornat <cornat.jacques@gmail.com>
2020-11-07 00:02:10 +0100

    increase post limit

--- app/middleware/post.ts
+++ app/middleware/post.ts
@@ -2,6 +2,6 @@ import * as express from 'express';
 import { Application } from 'express';
 
 export function init(app: Application): void {
-  app.use(express.urlencoded({extended: true}));
-  app.use(express.json());
+  app.use(express.urlencoded({limit: '2mb', extended: true}));
+  app.use(express.json({limit: '2mb'}));
 }
