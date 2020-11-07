83c5dfee7641bb924277b711aa150b07296bfafd
Jacques Cornat <cornat.jacques@gmail.com>
2020-11-08 00:51:19 +0100

    add header & footer support

--- app/controller/pdf.ts
+++ app/controller/pdf.ts
@@ -5,9 +5,12 @@ import * as PDF from '../model/pdf';
 export function init(app: Application): void {
   app.get('/api/convert', async (req: any, res: any, next: any) => {
     const html = req.query.html;
+    const options = {
+      content: html,
+    };
 
     try {
-      const data = await PDF.convertHtmlContentToPDF(html);
+      const data = await PDF.convertHtmlContentToPDF(options);
       res.send(data);
     } catch (error) {
       return next(error);
@@ -18,9 +21,12 @@ export function init(app: Application): void {
 
   app.post('/api/convert', async (req: any, res: any, next: any) => {
     const html = req.body.html;
+    const options = {
+      content: html,
+    };
 
     try {
-      const data = await PDF.convertHtmlContentToPDF(html);
+      const data = await PDF.convertHtmlContentToPDF(options);
       res.send(data);
     } catch (error) {
       return next(error);
--- app/model/pdf.ts
+++ app/model/pdf.ts
@@ -4,29 +4,38 @@ import * as puppeteer from 'puppeteer';
 
 import { URL } from '../config/config';
 import * as Directory from './directory';
+import * as Global from './global';
 
 export function randomInteger(min: number = 100000, max: number = 999999) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-export async function convertHtmlContentToPDF(content: string): Promise<string> {
+export async function convertHtmlContentToPDF(options: { content: string, headerTemplate?: string, footerTemplate?: string, margin?: { top: number, left: number, right: number, bottom: number } }): Promise<string> {
+  if (Global.isEmpty(options?.content)) {
+    throw {status: 400, message: `Empty content cannot be empty`};
+  }
+
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
 
-  await page.setContent(content);
+  await page.setContent(options.content);
 
   const currentDate = moment().format('MMDDHHmmsss');
   const random = randomInteger();
   const title = `${currentDate}-${random}.pdf`;
   const destination = path.join(__dirname, '..', 'public', 'pdf', title);
 
-  const options = {
+  const displayHeaderFooter = (Global.isPopulated(options.headerTemplate) || Global.isPopulated(options.footerTemplate));
+  const optionsPDF = {
     path: destination,
     format: 'A4',
-    margin: {top: 20, left: 20, right: 20, bottom: 20},
+    margin: {top: 100, left: 100, right: 100, bottom: 100},
+    displayHeaderFooter,
+    headerTemplate: options.headerTemplate,
+    footerTemplate: options.footerTemplate,
   };
 
-  await page.pdf(options);
+  await page.pdf(optionsPDF);
   await browser.close();
 
   return `${URL}/public/pdf/${title}`;
