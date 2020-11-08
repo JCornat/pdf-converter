df00ba06878ac55b82f2acabe9a5fcc259851da2
Jacques Cornat <cornat.jacques@gmail.com>
2020-11-08 23:32:51 +0100

    improve pdf generation

--- app/model/pdf.ts
+++ app/model/pdf.ts
@@ -11,7 +11,7 @@ export function randomInteger(min: number = 100000, max: number = 999999) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-export async function convertHtmlContentToPDF(options: { content: string, headerTemplate?: string, footerTemplate?: string, margin?: { top: number, left: number, right: number, bottom: number } }): Promise<string> {
+export async function convertHtmlContentToPDF(options: { content: string, headerTemplate?: string, footerTemplate?: string, style?: string, format?: string, landscape?: boolean, disableResetCSS?: boolean, width?: string | number, height?: string | number, margin?: { top?: number, left?: number, right?: number, bottom?: number } }): Promise<string> {
   if (Global.isEmpty(options?.content)) {
     throw {status: 400, message: `Empty content cannot be empty`};
   }
@@ -26,16 +26,72 @@ export async function convertHtmlContentToPDF(options: { content: string, header
   const title = `${currentDate}-${random}.pdf`;
   const destination = path.join(__dirname, '..', 'public', 'pdf', title);
 
-  const displayHeaderFooter = (Global.isPopulated(options.headerTemplate) || Global.isPopulated(options.footerTemplate));
-  const optionsPDF = {
-    path: destination,
-    format: 'A4',
-    margin: {top: 100, left: 100, right: 100, bottom: 100},
-    displayHeaderFooter,
-    headerTemplate: options.headerTemplate,
-    footerTemplate: options.footerTemplate,
+  const margin = {
+    top: options.margin?.top || 20,
+    left: options.margin?.left || 20,
+    right: options.margin?.right || 20,
+    bottom: options.margin?.bottom || 20,
   };
 
+  const resetCSS = fs.readFileSync(path.join(__dirname, '..', 'private', 'reset.css'), 'utf-8');
+  const defaultCSS = fs.readFileSync(path.join(__dirname, '..', 'private', 'default.css'), 'utf-8');
+  const headerCSS = fs.readFileSync(path.join(__dirname, '..', 'private', 'header.css'), 'utf-8');
+
+  let headerTemplate: string;
+  let footerTemplate: string;
+  const displayHeaderFooter = (Global.isPopulated(options.headerTemplate) || Global.isPopulated(options.footerTemplate));
+  if (displayHeaderFooter) {
+    const headerFooterCSS = `
+      <style>
+        ${resetCSS}
+        ${defaultCSS}
+        ${headerCSS}
+      </style>
+    `
+
+    if (Global.isPopulated(options.headerTemplate)) {
+      headerTemplate = `${headerFooterCSS}<header>${options.headerTemplate}</header>`;
+    }
+
+    if (Global.isPopulated(options.footerTemplate)) {
+      footerTemplate = `${headerFooterCSS}<footer>${options.footerTemplate}</footer>`;
+    }
+  }
+
+  let format;
+  let width;
+  let height;
+  if (Global.isPopulated(options.width) && Global.isPopulated(options.height)) {
+    width = options.width;
+    height = options.height;
+  } else {
+    format = options.format || 'A4';
+  }
+
+  const landscape = options.landscape || false;
+
+  const optionsPDF = {
+    path: destination,
+    format,
+    width,
+    height,
+    margin,
+    scale: 1,
+    landscape,
+    displayHeaderFooter,
+    headerTemplate,
+    footerTemplate,
+  };
+
+  if (Global.isEmpty(options.disableResetCSS)) {
+    await page.addStyleTag({content: `${resetCSS}${defaultCSS}`});
+  }
+
+  if (Global.isPopulated(options.style)) {
+    await page.addStyleTag({content: options.style});
+  }
+
+  await page.emulateMedia({media: 'screen'});
   await page.pdf(optionsPDF);
   await browser.close();
 
